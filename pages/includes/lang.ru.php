d1960d6479e597cf25699ff82c922db1b59d332c
Théo <julientheo@eisti.eu>
2021-06-24 11:37:44 +0200

    language for home and about

--- pages/includes/lang.ru.php
+++ pages/includes/lang.ru.php
@@ -1,4 +1,68 @@
 <?php
+/* Navbar */
+$lang["navbar"]["title"] = "Французские <span>а</span>ссоциативные <span>С</span>ловари";
+$lang["navbar"]["about"] = "О проекте";
+$lang["navbar"]["authors"] = "Авторы";
+$lang["navbar"]["dictionaries"] = "Словари";
+$lang["navbar"]["fas"] = "Французский Ассоциативный Словарь (ФАС)";
+$lang["navbar"]["sanf"] = "Словарь ассоциативных норм франкофонии (САНФ)";
+$lang["navbar"]["sanfn"] = "Словарь ассоциативных норм франкофонии нормализованый (САНФН)";
+$lang["navbar"]["fasn"] = "Словарь французских ассоциаций 2019 (ФАСН-2019)";
+$lang["navbar"]["fas1_red"] = "Словарь французских ассоциаций 2012+2019 (ФАС1-сокращенный)";
+$lang["navbar"]["fas2_red"] = "Словарь французских ассоциаций 2012+2019 (ФАС2-сокращенный)";
+$lang["navbar"]["help"] = "Помощь";
+
+/* Home */
+$lang["home"]["intro"] = "Ассоциативный словарь - это прекрасный источник, раскрывающий внутренний мир, проблемы и чаяния целого народа или его подгруппы.";
+$lang["home"]["fb"] = "Поделиться на Facebook";
+$lang["home"]["vk"] = "Поделиться в VK";
+$lang["home"]["linkedin"] = "Поделиться на Linkedin";
+
+/* About */
+$lang["about"]["fas"]["title"] = "Французский Ассоциативный Словарь (ФАС)";
+$lang["about"]["fas"]["description"] = "Данный словарь основан на ответах на вопросы анкет, заполненных в 2008-2010 годах. Исправлены
+										только орфография и пунктуация. Этот словарь позволяет нам изучать синтагматические связи между
+										стимулом и реакцией.";
+$lang["about"]["sanf"]["title"] = "Словарь ассоциативных норм франкофонии (САНФ)";
+$lang["about"]["sanf"]["description"] = "Данный словарь основан на ответах на анкеты, заполненные в 2013-2015 годах. Исправлены только
+										орфография и пунктуация. Этот словарь позволяет нам изучать синтагматические связи между
+										стимулом и реакцией.";
+$lang["about"]["sanfn"]["title"] = "Словарь ассоциативных норм франкофонии нормализованый (САНФН)";
+$lang["about"]["sanfn"]["description"] = "Данный словарь основан на ответах на вопросы анкет, заполненных в 2013-2015 годах. Помимо
+											исправления орфографии и пунктуации, ответы были унифицированы: ответы, выраженные в разных
+											формах (мужской - женский род прилагательного, отглагольные формы), объединены под одним
+											заголовочным словом, удалены артикли и предлоги. Этот словарь не показывает синтагматические
+											связи между стимулом и ответом, но он позволяет нам лучше выделить семантические связи между
+											ними, что особенно важно при составлении обратного словаря.";
+$lang["about"]["fasn"]["title"] = "Словарь французских ассоциаций 2019 (ФАСН-2019)";
+$lang["about"]["fasn"]["description"] = "Данный словарь составлен на основе ответов на анкеты, заполненные в 2018 году. Помимо исправления
+										орфографии и пунктуации, ответы были унифицированы: ответы, выраженные в разных формах (мужской
+										- женский род прилагательного, отглагольные формы), объединены под одним заголовочным словом,
+										удалены артикли и предлоги. Этот словарь не показывает синтагматические связи между стимулом и
+										ответом, но он позволяет нам лучше выделить семантические связи между ними, что особенно важно
+										при составлении обратного словаря.";
+$lang["about"]["fas1_red"]["title"] = "Словарь французских ассоциаций 2012+2019 (ФАС1-сокращенный)";
+$lang["about"]["fas1_red"]["description"] = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, vero vitae perspiciatis quod quos
+											fugit numquam doloremque fuga aperiam dolore, recusandae dolorum quidem modi laboriosam expedita
+											tempora, assumenda libero inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+											Provident, esse vel? Facere, praesentium quo aliquid animi, sequi maxime dolor non vitae
+											similique, cumque itaque atque maiores explicabo nulla nisi distinctio.";
+$lang["about"]["fas2_red"]["title"] = "Словарь французских ассоциаций 2012+2019 (ФАС2-сокращенный)";
+$lang["about"]["fas2_red"]["description"] = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit expedita ab cum. Sequi totam
+											hic blanditiis assumenda, itaque, atque autem, id eaque ullam harum quos delectus quibusdam
+											asperiores ipsum. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque quae atque
+											iure dicta consequatur modi quibusdam, excepturi tenetur iusto fuga minus ex. Cupiditate,
+											molestias. Tempore temporibus suscipit recusandae! Ab, tempora!";
 
 
+
+/* Others */
+$lang["path"]["home"] = "ГЛАВНАЯ";
+$lang["pages"]["title"] = "Французский Ассоциативный Словарь";
+$lang["dict"]["fas"] = "ФАС";
+$lang["dict"]["sanf"] = "САНФ";
+$lang["dict"]["sanfn"] = "САНФН";
+$lang["dict"]["fasn"] = "ФАСН-2019";
+$lang["dict"]["fas1_red"] = "ФАС1-сокращенный";
+$lang["dict"]["fas2_red"] = "ФАС2-сокращенный";
 ?>

a6f2e02ae994f1b54d03bd452834c50c5f04909f
Théo <julientheo@eisti.eu>
2021-06-24 11:45:41 +0200

    about with language tab

--- pages/about.php
+++ pages/about.php
@@ -32,53 +32,28 @@
 			<div class="content three_quarter">
 				<h1><?php echo $lang["pages"]["title"]; ?></h1>
 				<div id="dict1">
-					<h2><a href="./dict_fas.php">Французский Ассоциативный Словарь (ФАС)</a></h2>
-					<p>Данный словарь основан на ответах на вопросы анкет, заполненных в 2008-2010 годах. Исправлены
-						только орфография и пунктуация. Этот словарь позволяет нам изучать синтагматические связи между
-						стимулом и реакцией.</p>
+					<h2><a href="./dict_fas.php"><?php echo $lang["about"]["fas"]["title"]; ?></a></h2>
+					<p><?php echo $lang["about"]["fas"]["description"]; ?></p>
 				</div>
 				<div id="dict2">
-					<h2><a href="./dict_sanf.php">Словарь ассоциативных норм франкофонии (САНФ)</a></h2>
-					<p>Данный словарь основан на ответах на анкеты, заполненные в 2013-2015 годах. Исправлены только
-						орфография и пунктуация. Этот словарь позволяет нам изучать синтагматические связи между
-						стимулом и реакцией.</p>
+					<h2><a href="./dict_sanf.php"><?php echo $lang["about"]["sanf"]["title"]; ?></a></h2>
+					<p><?php echo $lang["about"]["sanf"]["description"]; ?></p>
 				</div>
 				<div id="dict3">
-					<h2><a href="./dict_sanfn.php">Словарь ассоциативных норм франкофонии нормализованый
-							(САНФН)</a></h2>
-					<p>Данный словарь основан на ответах на вопросы анкет, заполненных в 2013-2015 годах. Помимо
-						исправления орфографии и пунктуации, ответы были унифицированы: ответы, выраженные в разных
-						формах (мужской - женский род прилагательного, отглагольные формы), объединены под одним
-						заголовочным словом, удалены артикли и предлоги. Этот словарь не показывает синтагматические
-						связи между стимулом и ответом, но он позволяет нам лучше выделить семантические связи между
-						ними, что особенно важно при составлении обратного словаря.</p>
+					<h2><a href="./dict_sanfn.php"><?php echo $lang["about"]["sanfn"]["title"]; ?></a></h2>
+					<p><?php echo $lang["about"]["sanfn"]["description"]; ?></p>
 				</div>
 				<div id="dict4">
-					<h2><a href="./dict_fasn.php">Словарь французских ассоциаций 2019 (ФАСН-2019)</a></h2>
-					<p>Данный словарь составлен на основе ответов на анкеты, заполненные в 2018 году. Помимо исправления
-						орфографии и пунктуации, ответы были унифицированы: ответы, выраженные в разных формах (мужской
-						- женский род прилагательного, отглагольные формы), объединены под одним заголовочным словом,
-						удалены артикли и предлоги. Этот словарь не показывает синтагматические связи между стимулом и
-						ответом, но он позволяет нам лучше выделить семантические связи между ними, что особенно важно
-						при составлении обратного словаря.</p>
+					<h2><a href="./dict_fasn.php"><?php echo $lang["about"]["fasn"]["title"]; ?></a></h2>
+					<p><?php echo $lang["about"]["fasn"]["description"]; ?>.</p>
 				</div>
 				<div id="dict5">
-					<h2><a href="./dict_fas1_red.php">Словарь французских ассоциаций 2012+2019
-							(ФАС1-сокращенный)</a></h2>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, vero vitae perspiciatis quod quos
-						fugit numquam doloremque fuga aperiam dolore, recusandae dolorum quidem modi laboriosam expedita
-						tempora, assumenda libero inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Provident, esse vel? Facere, praesentium quo aliquid animi, sequi maxime dolor non vitae
-						similique, cumque itaque atque maiores explicabo nulla nisi distinctio.</p>
+					<h2><a href="./dict_fas1_red.php"><?php echo $lang["about"]["fas1_red"]["title"]; ?></a></h2>
+					<p><?php echo $lang["about"]["fas1_red"]["description"]; ?></p>
 				</div>
 				<div id="dict6">
-					<h2><a href="./dict_fas2_red.php">Словарь французских ассоциаций 2012+2019
-							(ФАС2-сокращенный)</a></h2>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit expedita ab cum. Sequi totam
-						hic blanditiis assumenda, itaque, atque autem, id eaque ullam harum quos delectus quibusdam
-						asperiores ipsum. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque quae atque
-						iure dicta consequatur modi quibusdam, excepturi tenetur iusto fuga minus ex. Cupiditate,
-						molestias. Tempore temporibus suscipit recusandae! Ab, tempora!</p>
+					<h2><a href="./dict_fas2_red.php"><?php echo $lang["about"]["fas2_red"]["title"]; ?></a></h2>
+					<p><?php echo $lang["about"]["fas2_red"]["description"]; ?></p>
 				</div>
 			</div>
 			<!-- / main body -->
